e105322008318382f94f2722a8606b2ece8d3a1e
Зайков Александр Сергеевич <zaykov.aleksandr@itmh.ru>
2016-06-27 17:52:47 +0500

    подключение к БД

--- book.lc/login.php
+++ book.lc/login.php
@@ -1,8 +1,4 @@
 <?php // login.php
-//$db_hostname = 'localhost';
-//$db_database = 'books';
-//$db_username = 'root';
-//$db_password = '1';
 
 $mysqli = new mysqli('localhost', 'root', '1', 'books');
 if ($mysqli->connect_error) {
