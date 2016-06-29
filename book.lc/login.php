ea0f5b908d70e498ca248aab4f70e3f6140a11b1
al285tpx <AlexZaikov@ya.ru>
2016-06-29 23:29:32 +0600

    Delete login.php

--- book.lc/login.php
+++
@@ -1,9 +0,0 @@
-<?php // login.php
-
-$mysqli = new mysqli('localhost', 'root', '1', 'books');
-if ($mysqli->connect_error) {
-    die('Ошибка подключения (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-}
-
-?>
